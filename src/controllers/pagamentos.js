69226b35c573327e63b7fadea2f19830956f747b
Alexandre Araujo <alexandre01393@gmail.com>
2022-03-23 10:19:39 -0300

    update: pagamento controller updated

--- src/controllers/pagamentos.js
+++ src/controllers/pagamentos.js
@@ -1,112 +1,122 @@
-import Pagamento from '../models/pagamentos.js'
-import User from "../models/user.js"
-import qr from 'qrcode'
+import Pagamento from "../models/pagamentos.js";
+import User from "../models/user.js";
+import qr from "qrcode";
 
 class PagamentoController {
-    static async createPagamento (req, res) {
-        try {
-            const {medidor, status} = req.body
-            const {id} = req.params
-            const user = await User.findById(id)
+  static async createPagamento(req, res) {
+    try {
+      const { medidor, status } = req.body;
+      const { id } = req.params;
+      const user = await User.findById(id);
 
-            const pix = await Pagamento.create({
-                idAssociado: user.id,
-                name: user.name,
-                medidor,
-                valor: 18 * 2,
-                status,
-                qrCode: await qr.toDataURL('https://www.youtube.com/')
-            })
-            const newList = [...user.historico_pagamentos, pix]
+      const pix = await Pagamento.create({
+        idAssociado: user.id,
+        name: user.name,
+        medidor,
+        valor: 18 * 2,
+        status,
+        qrCode: await qr.toDataURL("https://www.youtube.com/"),
+      });
+      const newList = [...user.historico_pagamentos, pix];
 
-            user.historico_pagamentos = newList
+      user.historico_pagamentos = newList;
 
-            await User.findByIdAndUpdate(id, {
-                historico_pagamentos: newList,
-                new: true
-            }, {
-                returnDocument: 'after'
-            })
-
-            return  res.status(201).json(pix)
-        } catch (error) {
-            return res.status(500).json({'erro': 'algo deu errado'})
+      await User.findByIdAndUpdate(
+        id,
+        {
+          historico_pagamentos: newList,
+          new: true,
+        },
+        {
+          returnDocument: "after",
         }
+      );
+
+      return res.status(201).json(pix);
+    } catch (error) {
+      return res.status(500).json({ erro: "algo deu errado" });
     }
-    static async findAll (req, res) {
-        try {
-            
-            const pix = await Pagamento.find()
+  }
+  static async findAll(req, res) {
+    try {
+      const pix = await Pagamento.find();
 
-            return  res.json(pix)
+      return res.json(pix);
+    } catch (error) {
+      return res.status(500).json({ erro: "algo deu errado" });
+    }
+  }
 
-        } catch (error) {
-            return res.status(500).json({'erro': 'algo deu errado'})
+  static async findOne(req, res) {
+    try {
+      const { id } = req.params;
+      const pagamento = await Pagamento.findById(id);
+
+      return res.json(pagamento);
+    } catch (error) {
+      return res.status(500).json({ erro: "algo deu errado" });
+    }
+  }
+
+  static async updatePagamento(req, res) {
+    try {
+      const { id } = req.params;
+      const { status } = req.body;
+
+      const updatePagamento = await Pagamento.findByIdAndUpdate(
+        id,
+        {
+          status,
+          new: true,
+        },
+        {
+          returnDocument: "after",
         }
+      );
+     await this.updateHistorico(updatePagamento);
+
+      return res.json(updatePagamento);
+    } catch (error) {
+      return res.status(500).json({ erro: error.message });
     }
+  }
 
-    static async findOne (req, res) {
-        try {
-            const {id} = req.params
-            const pagamento = await Pagamento.findById(id)
+  static async deletePagamento(req, res) {
+    try {
+      const { id } = req.params;
 
-            return res.json(pagamento)
+      await Pagamento.findByIdAndDelete(id);
 
-        } catch (error) {
-            return res.status(500).json({'erro': 'algo deu errado'})
-        }
+      return res.json({});
+    } catch (error) {
+      return res.status(500).json({ erro: "algo deu errado" });
     }
+  }
 
-    static async updatePagamento (req, res) {
-        try {
-            const {id} = req.params
-            const {status} = req.body
+  static async updateHistorico(newPagamento) {
+    try {
+      const { historico_pagamentos } = await User.findById(
+        newPagamento.idAssociado
+      );
 
-            const updatePagamento = await Pagamento.findByIdAndUpdate(id, {
-                status,
-                new: true
-            }, {
-                returnDocument: 'after'
-            })
+      const pagamentoUpdated = historico_pagamentos.map((item) => {
+         
+          if (item._id.toString() === newPagamento.id) {
+              item.status = "Pago"
+            }
+            return item
+        })
+        
+        await User.findByIdAndUpdate(newPagamento.idAssociado, {
+            historico_pagamentos: [...pagamentoUpdated],
+            new: true
+        });
 
-            this.updateHistorico(updatePagamento)
 
-            return  res.json(updatePagamento)
-            
-        } catch (error) {
-            return  res.status(500).json({'erro': error.message})
-        }
+    } catch (error) {
+      return "Algo deu errado";
     }
-
-    static async deletePagamento (req, res) {
-        try {
-            
-            const {id} = req.params
-
-            await Pagamento.findByIdAndDelete(id)
-
-            return res.json({})
-
-        } catch (error) {
-            return  res.status(500).json({'erro': 'algo deu errado'})
-        }
-    }
-
-    static async updateHistorico (newPagamento) {
-        try {
-            const {historico_pagamentos} = await User.findById(newPagamento.idAssociado)
-
-            const antigoPagamento = historico_pagamentos.findIndex(({id}) => id === newPagamento.id)
-
-            historico_pagamentos.splice(antigoPagamento, 1)
-
-            await User.findByIdAndUpdate(idAssociado, {
-                historico_pagamentos: [...historico_pagamentos, newPagamento]
-            }) 
-        } catch (error) {
-            return "Algo deu errado"
-        }
-    }
+  }
 }
 
-export default PagamentoController
+export default PagamentoController;
